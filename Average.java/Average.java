c0fea551cfcfc4aab46a5aa3553100c06bb0e306
Mohaimenul <mahadihasan1603@gmail.com>
2022-08-17 23:20:13 +0600

    Java Average

---
+++ Average.java/Average.java
@@ -0,0 +1,13 @@
+public class Average{
+    public static void main(String[] args){
+	double[] numArray = { 45.5,67.5,-45.6,20.34, 33.0,45.6};
+	double sum = 0.0;
+	
+	for (double num: numArray) {
+	   sum += num;
+	 }
+	 double average = sum / numArray.length;
+	 System.out.format("the average is : %.2f", average);
+	 }
+	 }
+	 
