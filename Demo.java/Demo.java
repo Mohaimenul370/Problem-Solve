d9ad28973715b6488c2bee7bbf5d45133064d27c
Mohaimenul <mahadihasan1603@gmail.com>
2022-08-19 22:49:25 +0600

    Demo Java

---
+++ Demo.java/Demo.java
@@ -0,0 +1,18 @@
+class Demo{
+      int value1;
+	  int value2;
+	  Demo(){
+	        value1 = 10;
+			value2 = 20;
+			System.out.prinln("inside constructor");
+			}
+			public void display(){
+			System.out.println("Value1 === "+ value1);
+			System.out.println("value2 === "+ value2);
+			}
+			public void display(){
+			 System.out.println(Sting[] args){
+			 Demo d1 = new Demo();
+			 d1.display();
+			 }
+			} 
