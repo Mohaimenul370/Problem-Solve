7924c5d428fab61ac00c0d402dc1feae5194781d
Mohaimenul <mahadihasan1603@gmail.com>
2022-08-20 23:20:02 +0600

    Data Type Java

---
+++ datatype.java/datatype.java
@@ -0,0 +1,24 @@
+public class datatype{
+
+   public static void main(String [] args){
+   
+     char c;
+	 boolean b;
+	 int i=440;
+	 float f=4958.5f;
+	 double d=546.2f;
+	  
+	  b = true;
+	   
+	 //  c = afdhafoi;
+	   
+	   System.out.println("the value of i="+i);
+	   System.out.println("The value of b is="+b);
+	   System.out.println("The value of is ="+f);
+	   System.out.println("d="+d);
+	   
+	   
+   
+   }
+
+}
