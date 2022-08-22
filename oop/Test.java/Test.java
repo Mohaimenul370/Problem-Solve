5edefa69b28686cfd9a712f420b012f5d17e5487
Mohaimenul <mahadihasan1603@gmail.com>
2022-08-22 21:06:09 +0600

    Object Oriented Programming-1

---
+++ oop/Test.java/Test.java
@@ -0,0 +1,13 @@
+public class Test{
+    public static void main(String[] args)
+	 {
+	 Teacher teacher1=new Teacher();
+      teacher1.name = "A";
+       teacher1.gender = "male";
+       teacher1.photo = "90855";
+
+      System.out.println("Name: "+teacher1.name);
+       System.out.println("Gender: "+teacher1.gender);
+	   
+	 }
+	 }
