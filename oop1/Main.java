a92fb065239b5ee1745fc73ca742751863d4b6f8
Mohaimenul <mahadihasan1603@gmail.com>
2022-09-10 23:07:15 +0600

    OOP

---
+++ oop1/Main.java
@@ -0,0 +1,15 @@
+public class Main{
+
+   public static void main(String[] args)
+   {
+      Mammal M = new Mammal();
+	  M.setName=(tiger);
+	  M.setNumber=(34);
+	  M.setNumber3(40);
+	  
+	  System.out.println(M.getName());
+	  System.out.println(M.getNumber());
+	  System.out.println(M.getNumber3());
+   
+   }
+}
---
+++ oop1/Mammal.java
@@ -0,0 +1,11 @@
+public class Mummal extends Animal{
+   private int number1;
+   
+   
+  public int getNumber3(){
+  return number1;
+  }
+  public void setNumber3(int number1){
+  this.number1=number1;
+  }
+}
