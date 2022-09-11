0e678d59c475f8d7a40e0401c8c103a2fed52ce9
Mohaimenul <mahadihasan1603@gmail.com>
2022-09-11 23:42:24 +0600

    Data Type

---
+++ Data-type.java/Data type.java
@@ -0,0 +1,18 @@
+public class Data type{
+
+public static void main(String [] args){
+   
+   booleen b;
+   char a;
+   short s;
+   int i;
+   floot f;
+   double d;
+   
+        b=true;
+		System.out.println("b");
+  
+
+  }
+
+}
