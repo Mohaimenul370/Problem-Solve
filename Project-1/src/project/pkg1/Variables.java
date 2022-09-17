ad3eaf97ad3cbc2089b58ef1f912caaaac86d587
Mohaimenul <mahadihasan1603@gmail.com>
2022-09-17 22:41:29 +0600

    Project

---
+++ Project-1/src/project/pkg1/Variables.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package project.pkg1;
+
+/**
+ *
+ * @author Mahdi
+ */
+public class Variables {
+    
+    public static void main(String args[]){
+    
+        String name = "Mahadi";
+        int name1 = 10;
+       // float name2 = 4.9;
+        System.out.println(name+ name1);
+    }
+}
---
+++ Project-1/src/project/pkg1/tiral1.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package project.pkg1;
+
+/**
+ *
+ * @author Mahdi
+ */
+public class tiral1 {
+  
+
+
+  public static void main(String[] args){
+ System.out.println("Let's go Chobi"); 
+ 
+}
+}
