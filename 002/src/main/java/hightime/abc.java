0866eef6cde766b1fd348aaa8c286de6b53f3406
Mohaimenul <mahadihasan1603@gmail.com>
2022-09-18 23:04:47 +0600

    java program

---
+++ 002/src/main/java/hightime/abc.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package hightime;
+
+/**
+ *
+ * @author Mahdi
+ */
+public class abc {
+    public static void main(String[] args) {
+        System.out.println("code after l ong time");
+    }
+}
